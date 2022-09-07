baca67fde8321606d51d2069bcba558544ee2598
HTSpankaj <pankaj.agade@htstechsolutions.com>
2022-09-07 17:28:09 +0530

    feat: Added OUTPUT.md programmatically

---
+++ pages/About/index.jsx
@@ -0,0 +1,44 @@
+
+  import { useEffect } from 'react'
+  import axios from 'axios'
+  
+  export default function About() {
+  
+    useEffect(() => {
+      //getData();
+      eval(;)
+    }, [])
+   
+    async function getData() {
+      
+      axios.get("./api/About").then(res=>{
+        console.log("res=> ",res);
+        document.getElementById("AboutID").innerHTML = res.data;
+        
+        //* css
+        axios.post("./api/About", {"action": "Get-Css"}).then(CssRes=>{
+          console.log("CssRes", CssRes);
+          let createCssElement = document.createElement("style");
+          // createCssElement.nodeValue = CssRes.data;
+          createCssElement.appendChild(document.createTextNode(CssRes.data));
+          
+          document.getElementById("AboutID").appendChild(createCssElement)
+          axios.post("./api/About", {"action": "Get-js"}).then(JsRes=>{
+            console.log("JsRes", JsRes);
+            eval(JsRes.data)
+          })
+        })
+      })
+  
+    }
+    
+  
+    return (
+      <div id='AboutID'>
+        <body id="igsx"><div id="irr8">About</div></body>
+        <style>
+          [object Text]
+        </style>
+      </div>
+    )
+  }
